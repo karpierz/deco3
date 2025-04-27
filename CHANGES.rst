b773648cfc3551d7f753057a56059957e9381fad
Adam Karpierz <akarpierz@gmail.com>
2025-04-27 15:35:41 +0200

    0.6.3 (2025-04-24) ------------------ - Initial commit.

---
+++ CHANGES.rst
@@ -0,0 +1,8 @@
+Changelog
+=========
+
+Above are (will be) changes of the original (v.0.6.3) *deco*:
+
+0.6.3 (2025-04-24)
+------------------
+- Initial commit.
---
+++ examples/free.py
@@ -0,0 +1,17 @@
+from deco import concurrent
+
+BODIES = [90]
+
+
+def run():
+    BODIES.append(210)
+    simulate()
+    simulate.wait()
+
+
+@concurrent
+def simulate():
+    print(BODIES)
+
+if __name__ == "__main__":
+    run()
